02fea5c01052e4e8be984430ff21a24d889877ea
Muhammed Jaseer <mjaseer43@gmail.com>
2023-11-09 10:02:12 +0530

    small changes

--- src/middlewares/validation/yup.ts
+++ src/middlewares/validation/yup.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { Validator } from '../../helpers/yupValidators';
 
 
-export class validationMiddleware {
+export class validation {
 
     private stringSchema!: Validator
 
@@ -10,7 +10,7 @@ export class validationMiddleware {
         this.stringSchema = new Validator()
     }
 
-    registerValidationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    registerValidation = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { email,password, firstName, lastName, phone } = req.body
 
@@ -30,7 +30,7 @@ export class validationMiddleware {
         }
     }
 
-    loginValidationMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    loginValidation = async (req: Request, res: Response, next: NextFunction) => {
         try {
             const { email,password } = req.body
 
@@ -44,6 +44,25 @@ export class validationMiddleware {
                 res.status(422).json({ error: error.message, message: 'Validation error' });
             } 
             else res.status(422).json({ error, message: 'Validation error' });
+        }
+    }
+
+    productValidation = async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            const { email,password, firstName, lastName, phone } = req.body
+
+            await this.stringSchema.emailSchema.validate(email)
+            await this.stringSchema.passwordSchema.validate(password)
+            await this.stringSchema.strinSchema.validate(firstName)
+            await this.stringSchema.strinSchema.validate(lastName)
+            await this.stringSchema.phoneNumberSchema.validate(phone)
+
+            next();
+        } catch (error) {
+            if (error instanceof Error) {
+                console.log(error.message)
+                res.status(422).json({ error: error.message, message: 'Validation error' });
+            } else res.status(422).json({ error, message: 'Validation error' });
         }
     }
 } 
--- src/router/user/user-router.ts
+++ src/router/user/user-router.ts
@@ -1,15 +1,15 @@
 import { Router } from "express";
 import { User } from "../../controllers/user/user";
-import { validationMiddleware } from "../../middlewares/validation/yup";
+import { validation } from "../../middlewares/validation/yup";
 
 const user = new User();
 
-const validator = new validationMiddleware()
+const validator = new validation()
 
 const router = Router()
 
-router.post('/register',validator.registerValidationMiddleware,user.userRegister)
+router.post('/register',validator.registerValidation,user.userRegister)
 
-router.post('/login',validator.loginValidationMiddleware,user.userLogin)
+router.post('/login',validator.loginValidation,user.userLogin)
 
 export default router
